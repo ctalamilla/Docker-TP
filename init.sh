ed693e110fad2e5a547798b36df969a548d01214
Cristian Patricio Salinas Talamilla <ctalamilla@MacBook-Pro-de-Cristian.local>
2022-05-12 22:05:51 -0300

    ejecicio terminado

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ docker build -t pythonitba .
 docker-compose up -d
 docker run -d --name itba  pythonitba sleep 1000
 docker network connect docker_default itba 
-docker exec -it itba bin/bash    
+docker exec -it itba bash -c "cd /filltables;python3 filltables.py"     
