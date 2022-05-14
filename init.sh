3d2b8966e4a685c5438c9d5dae30859a546303e5
Cristian Patricio Salinas Talamilla <ctalamilla@MacBook-Pro-de-Cristian.local>
2022-05-14 19:31:55 -0300

    comenzando ejercicio 5

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ docker build -t pythonitba .
 docker-compose up -d
 datapath=$(readlink -f data/datosDelitos.csv)
 docker run -d --name itba -v $datapath:/datain/datosDelitos.csv pythonitba sleep 1000
-docker network connect tpf-foundations-ctalamilla_default itba 
+docker network connect db_delitos_default itba 
 docker exec -it itba bash -c "cd /filltables;python3 filltables.py"     
--- python/filltables.py
+++ python/filltables.py
@@ -3,6 +3,6 @@ import sqlalchemy
 
 datos = pd.read_csv('/datain/datosDelitos.csv', index_col=0 )
 # Create the engine to connect to the PostgreSQL database
-engine = sqlalchemy.create_engine('postgresql://postgres:postgres@tpf-foundations-ctalamilla_postgres_1:5432/delitos')
+engine = sqlalchemy.create_engine('postgresql://postgres:postgres@db_delitos:5432/delitos')
 # Write data into the table in PostgreSQL database
 datos.to_sql('delitos',engine, if_exists= 'append')
